1c23a8a6eb80ba60c27966a4d992b041284061d6
Bobby Wibowo <bobbywibowo98@gmail.com>
2019-01-31 16:44:04 +0700

    Updated cfpurge.js
    This will now intended for manually purging uploads only.

--- scripts/cfpurge.js
+++ scripts/cfpurge.js
@@ -1,32 +1,27 @@
-// This is sorta no longer in use since lolisafe.js will do this automatically
-// everytime is launches.
-
 const { stripIndents } = require('./_utils')
 const utils = require('./../controllers/utilsController')
-const config = require('./../config')
 
 const cfpurge = {}
 
 cfpurge.do = async () => {
   const location = process.argv[1].replace(process.cwd() + '/', '')
   const args = process.argv.slice(2)
-  if (args.includes('--help') || args.includes('-h'))
+  if (!args.length || args.includes('--help') || args.includes('-h'))
     return console.log(stripIndents(`
       Purge Cloudflare's cache.
 
-      Usage:\nnode ${location} [...filename]
+      Usage:\nnode ${location} ...filename
 
       filename:
-      File names (as in uploads) separated by space (will automatically include their thumbs if available).
-      If not provided, this will default to frontend pages listed in "pages" array in config.js.
+      Upload names separated by space (will automatically include their thumbs if available).
     `))
 
-  const filenames = args.length ? args : config.pages.concat(['api/check'])
-  const result = await utils.purgeCloudflareCache(filenames, Boolean(args.length))
-  if (result.errors.length)
-    return result.errors.forEach(error => console.error(`CF: ${error}`))
-  else
-    console.log(`URLs:\n${result.files.join('\n')}\n\nSuccess: ${result.success}`)
+  const results = await utils.purgeCloudflareCache(args, true)
+  for (const result of results)
+    if (result.errors.length)
+      result.errors.forEach(error => console.error(`CF: ${error}`))
+    else
+      console.log(`URLs:\n${result.files.join('\n')}\n\nSuccess: ${result.success}`)
 }
 
 cfpurge.do()
